4143a8d51d86a6d585d472461728a5b25c01c18a
Michail_N <mihasik323@gmail.com>
2021-09-06 17:46:22 +0300

    added mysql2 package, removed static files

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const express = require('express');
+const http = require("http");
+const express = require("express");
+const WebSocket = require("ws");
 
 const app = express();
+const server = http.createServer(app);
 
-app.set('view engine', 'ejs');
+const webSocketServer = new WebSocket({ server });
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+webSocketServer.on("connection", (ws) => {
+  ws.on("message", (m) => {
+    webSocketServer.clients.forEach((client) => client.send(m));
+  });
 
-app.get('/', (req, res) => {
-    res.render('chat');
+  ws.on("error", (e) => ws.send(e));
+
+  ws.send("Hi there, I am a WebSocket server");
 });
 
-app.listen(3000);
+
+// app.use(express.urlencoded({ extended: true }));
+
+app.get("/chat", (req, res) => {
+  res.render("chat");
+});
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.post("/authorization", (req, res) => {
+  const userData = req.body;
+  console.log(userData);
+
+  res.redirect("/chat");
+});
+
+app.use("/", (req, res) => {
+  res.render("404");
+});
+
+server.listen(5000);
+
+// const connection = require('./utils/db');
+
+// connection.connect(error=>{
+//   if(error) console.log(error);
+//   else console.log('success');
+// })
