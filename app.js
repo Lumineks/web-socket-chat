e18074a567b538d3b6afa50ef1326bed50674d1b
Michail_N <mihasik323@gmail.com>
2021-09-06 13:12:13 +0300

    folder structure, basic server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('chat');
+});
+
+app.listen(3000);
